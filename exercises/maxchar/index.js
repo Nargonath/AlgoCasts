cecc2f4b848ee817b833fe8eb1b6e28a4eb258c7
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-16 23:46:38 +0100

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const map = new Map();
+
+  for (let char of str) {
+    const count = map.has(char) ? map.get(char) + 1 : 1;
+    map.set(char, count);
+  }
+
+  let maxEntry = null;
+  for (let [char, count] of map) {
+    if (!maxEntry) {
+      maxEntry = [char, count];
+    } else if (maxEntry[1] < count) {
+      maxEntry = [char, count];
+    }
+  }
+
+  return maxEntry[0];
+}
 
 module.exports = maxChar;
