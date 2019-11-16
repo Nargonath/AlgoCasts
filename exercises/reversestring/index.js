0f7212a2995ebc676a7c995e03e0d5c2903a0a72
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-16 15:00:19 +0100

    solve reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((acc, char) => `${char}${acc}`, "");
+}
 
 module.exports = reverse;
+
+// first solution
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str.charAt(i);
+//   }
+
+//   return reversed;
+// }
+
+// second solution
+// function reverse(str) {
+//   return Array.from(str)
+//     .reverse()
+//     .join("");
+// }
