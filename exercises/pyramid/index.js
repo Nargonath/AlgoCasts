f67e3777ca27a76b54519fadcf414506d42e4689
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-17 22:16:26 +0100

    solve pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,57 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(depth, currentRow = 0, pyramidLevel = "") {
+  if (depth === currentRow) {
+    return;
+  }
+
+  const totalChars = 2 * depth - 1;
+  if (pyramidLevel.length === totalChars) {
+    console.log(pyramidLevel);
+    pyramid(depth, currentRow + 1);
+    return;
+  }
+
+  const currentCol = pyramidLevel.length;
+  const pyramidMidPoint = Math.floor(totalChars / 2);
+  let charToAdd = "";
+
+  if (
+    pyramidMidPoint - currentRow <= currentCol &&
+    pyramidMidPoint + currentRow >= currentCol
+  ) {
+    charToAdd += "#";
+  } else {
+    charToAdd += " ";
+  }
+
+  pyramid(depth, currentRow, pyramidLevel + charToAdd);
+}
 
 module.exports = pyramid;
+
+// first solution
+/* function pyramid(depth) {
+  // 2n+1
+  // n = 0, => 1
+  // n = 1, => 3
+  // n = 2, => 5
+  // n = 3, => 7
+
+  const totalChar = 2 * depth - 1;
+  const pyramidMiddle = Math.floor(totalChar / 2);
+  for (let row = 0; row < depth; row++) {
+    let rowString = "";
+
+    for (let col = 0; col < totalChar; col++) {
+      if (pyramidMiddle - row <= col && pyramidMiddle + row >= col) {
+        rowString += '#';
+      } else {
+        rowString += ' ';
+      }
+    }
+
+    console.log(rowString);
+  }
+} */
