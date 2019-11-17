c00c91b363c580055f7dd5c67349588fd7943969
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-17 22:41:54 +0100

    solve vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// first solution
+/* function vowels(str) {
+  const vowelsList = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (vowelsList.includes(char)) {
+      count += 1;
+    }
+  }
+
+  return count;
+} */
