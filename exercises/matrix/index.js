076cf25f6cb556964eb9dcdd4ec5124df0b9719f
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-18 20:15:02 +0100

    solve matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  let count = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  for (let i = 0; i < n; i++) {
+    // not needed because you can write into an array at a position i
+    // even if it was not initialized like below however the solution
+    // below is more readible
+    results.push(Array(n));
+  }
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for (let currentCol = startColumn; currentCol <= endColumn; currentCol++) {
+      results[startRow][currentCol] = count;
+      count++;
+    }
+    startRow++;
+
+    for (let currentRow = startRow; currentRow <= endRow; currentRow++) {
+      results[currentRow][endColumn] = count;
+      count++;
+    }
+    endColumn--;
+
+    for (let currentCol = endColumn; currentCol >= startColumn; currentCol--) {
+      results[endRow][currentCol] = count;
+      count++;
+    }
+    endRow--;
+
+    for (let currentRow = endRow; currentRow >= startRow; currentRow--) {
+      results[currentRow][startColumn] = count;
+      count++;
+    }
+    startColumn++;
+  }
+
+  return results;
+}
 
 module.exports = matrix;
