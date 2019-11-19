99218b7dd1ae92ae688e9c6b2fbcf1c613cc29fd
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-19 20:05:06 +0100

    solve linkedlist until removeAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,104 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, nextNode = null) {
+    this.data = data;
+    this.next = nextNode;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let count = 0;
+    let currentNode = this.head;
+
+    while (currentNode) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let last = this.head;
+
+    while (last && last.next) {
+      last = last.next;
+    }
+
+    return last;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    let currentNode = this.head;
+    let previousNode = null;
+
+    while (currentNode && currentNode.next) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+
+    if (!previousNode) {
+      this.head = null;
+    } else {
+      previousNode.next = null;
+    }
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    const newNode = new Node(data);
+    if (!last) {
+      this.head = newNode;
+      return;
+    }
+
+    last.next = newNode;
+  }
+
+  getAt(index) {
+    if (typeof index !== "number") {
+      throw new TypeError("index should be a number");
+    }
+
+    let currentNode = this.head;
+    let counter = 0;
+    while (currentNode && counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    if (counter < index) {
+      return null;
+    }
+    return currentNode;
+  }
+}
 
 module.exports = { Node, LinkedList };
