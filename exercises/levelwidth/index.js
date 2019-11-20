7d477213049a65ff88395770d7ad72335ffa2a18
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-11-20 19:45:08 +0100

    solve levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,45 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// his solution which is I think better in term of Runtime complexity
+function levelWidth(root) {
+  const STOPPER = "stop";
+  const nodes = [root, STOPPER];
+  const result = [0];
+
+  while (nodes.length > 1) {
+    const currentNode = nodes.shift(nodes);
+    if (currentNode === STOPPER && nodes.length) {
+      nodes.push(STOPPER);
+      result.push(0);
+    } else {
+      nodes.push(...currentNode.children);
+      result[result.length - 1]++;
+    }
+  }
+
+  return result;
+}
 
 module.exports = levelWidth;
+
+// my personal solution
+/* function flatten(array) {
+  return array.reduce((acc, current) => acc.concat(current), []);
+}
+
+function levelWidth(root) {
+  let nodes = [root];
+  let result = [1];
+
+  while (nodes.length) {
+    const allChildrenLevel = flatten(nodes.map(node => node.children));
+    nodes.splice(0);
+    nodes.push(...allChildrenLevel);
+    if (allChildrenLevel.length > 0) {
+      result.push(allChildrenLevel.length);
+    }
+  }
+
+  return result;
+} */
